dae667ff0238027f018f9a470defc0b43c97e04a
liudijiang <474147597@qq.com>
2016-06-22 16:32:25 +0800

    deleted:    API.txt 	deleted:    LICENSE 	deleted:    README.md 	deleted:    Sources/cpu/STM32F429.lib 	deleted:    Sources/cpu/arm_common_tables.h 	deleted:    Sources/cpu/arm_const_structs.h 	deleted:    Sources/cpu/arm_math.h 	deleted:    Sources/cpu/core_cm4.h 	deleted:    Sources/cpu/core_cmFunc.h 	deleted:    Sources/cpu/core_cmInstr.h 	deleted:    Sources/cpu/core_cmSimd.h 	deleted:    Sources/cpu/misc.h 	deleted:    Sources/cpu/startup_stm32f429_439xx.s 	deleted:    Sources/cpu/stm32f4xx.h 	deleted:    Sources/cpu/stm32f4xx_adc.h 	deleted:    Sources/cpu/stm32f4xx_can.h 	deleted:    Sources/cpu/stm32f4xx_cec.h 	deleted:    Sources/cpu/stm32f4xx_conf.h 	deleted:    Sources/cpu/stm32f4xx_crc.h 	deleted:    Sources/cpu/stm32f4xx_cryp.h 	deleted:    Sources/cpu/stm32f4xx_dac.h 	deleted:    Sources/cpu/stm32f4xx_dbgmcu.h 	deleted:    Sources/cpu/stm32f4xx_dcmi.h 	deleted:    Sources/cpu/stm32f4xx_dma.h 	deleted:    Sources/cpu/stm32f4xx_dma2d.h 	deleted:    Sources/cpu/stm32f4xx_dsi.h 	deleted:    Sources/cpu/stm32f4xx_exti.h 	deleted:    Sources/cpu/stm32f4xx_flash.h 	deleted:    Sources/cpu/stm32f4xx_flash_ramfunc.h 	deleted:    Sources/cpu/stm32f4xx_fmc.h 	deleted:    Sources/cpu/stm32f4xx_fmpi2c.h 	deleted:    Sources/cpu/stm32f4xx_fsmc.h 	deleted:    Sources/cpu/stm32f4xx_gpio.h 	deleted:    Sources/cpu/stm32f4xx_hash.h 	deleted:    Sources/cpu/stm32f4xx_i2c.h 	deleted:    Sources/cpu/stm32f4xx_iwdg.h 	deleted:    Sources/cpu/stm32f4xx_lptim.h 	deleted:    Sources/cpu/stm32f4xx_ltdc.h 	deleted:    Sources/cpu/stm32f4xx_pwr.h 	deleted:    Sources/cpu/stm32f4xx_qspi.h 	deleted:    Sources/cpu/stm32f4xx_rcc.h 	deleted:    Sources/cpu/stm32f4xx_rng.h 	deleted:    Sources/cpu/stm32f4xx_rtc.h 	deleted:    Sources/cpu/stm32f4xx_sai.h 	deleted:    Sources/cpu/stm32f4xx_sdio.h 	deleted:    Sources/cpu/stm32f4xx_spdifrx.h 	deleted:    Sources/cpu/stm32f4xx_spi.h 	deleted:    Sources/cpu/stm32f4xx_syscfg.h 	deleted:    Sources/cpu/stm32f4xx_tim.h 	deleted:    Sources/cpu/stm32f4xx_usart.h 	deleted:    Sources/cpu/stm32f4xx_wwdg.h 	deleted:    Sources/driver/board.c 	deleted:    Sources/driver/board.h 	deleted:    Sources/driver/gpio.c 	deleted:    Sources/driver/gpio.h 	deleted:    Sources/driver/nvic.c 	deleted:    Sources/driver/nvic.h 	deleted:    Sources/driver/uart.c 	deleted:    Sources/driver/uart.h 	deleted:    Sources/kernel/cpu_init.c 	deleted:    Sources/kernel/kernel-cm3.lib 	deleted:    Sources/kernel/kernel-cm4.lib 	deleted:    Sources/kernel/kernel.h 	deleted:    Sources/main/debug.c 	deleted:    Sources/main/debug.h 	deleted:    Sources/main/main.c 	deleted:    demo.uvprojx 	deleted:    text.txt

--- Sources/driver/board.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BOARD_H
-#define __BOARD_H
-
-void board_init(void);
-void board_power_on(void);
-void board_power_off(void);
-void board_reset(void);
-
-#endif
--- Sources/main/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __debug_h__
-#define __debug_h__
-
-void debug(const char* fmt, ...)
-void debug_init(void);
-
-#endif
-
-
